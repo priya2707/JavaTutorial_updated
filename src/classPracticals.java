4a541d07fd9c9915d669656c9e350a16a4f92231
priya2707 <kanagaraj.rathnapriya@gmail.com>
2020-12-07 22:30:15 +0100

    addNumbers Function added

--- src/classPracticals.java
+++ src/classPracticals.java
@@ -1,4 +1,20 @@
 public class classPracticals {
 
-    
+public static void main (String... hhh)
+    {
+        classPracticals o = new classPracticals();
+        int total1= o.addNumbers(5,7);
+        int total2=o.addNumbers(4,9);
+        System.out.println("The Sum of two numbers:"+ total1);
+        System.out.println("The Sum of other two numbers:"+ total2);
+    }
+
+ public int addNumbers(int a, int b)
+ {
+     int sum = a + b;
+     return sum;
+ }
+
+
+
 }
